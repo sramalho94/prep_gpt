cadca2aeec1d4000c7fcffae771f7019cf35c398
Stephan D Ramalho <sramalho@fordham.edu>
2023-09-20 15:34:58 -0400

    imported form to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import StartForm from '@/components/StartForm'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen min-w-screen flex-col justify-center p-24"></main>
+    <main className="flex min-h-screen min-w-screen flex-col justify-center p-24">
+      <StartForm />
+    </main>
   )
 }
